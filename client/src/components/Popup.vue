d8a04fb00ba398fa0142a17d87a1ca0ca1827bff
bcoggins78 <bcoggins78@gmail.com>
2019-06-10 02:00:20 -0400

    Created placeholder popup for contact

---
+++ client/src/components/Popup.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-dialog max-width="600px">
+    
+      <v-btn flat slot="activator">Contact</v-btn>
+    
+    <v-card>
+      <v-card-title>
+        <h2>Contact Me</h2>
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
