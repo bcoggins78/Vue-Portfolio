4055302a6075e0890bdc641c003ac2f0b6ee3eec
bcoggins78 <bcoggins78@gmail.com>
2019-06-10 02:01:30 -0400

    Added navbar and footer components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view/>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+export default {
+  name: "App",
+  components: { Navbar,
+                Footer },
+  data() {
+    return {
+
+    }
+  }
+  
+};
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
